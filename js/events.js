50655c239a9bec44a771266e71cd150f45a87c43
mharif1 <md5667676@gmail.com>
2022-08-14 19:10:12 +0600

    first commit

---
+++ js/events.js
@@ -0,0 +1,24 @@
+function makeCrimson() {
+    document.body.style.backgroundColor = 'crimson'
+}
+function makeShada() {
+    document.body.style.backgroundColor = 'white'
+}
+function makeBlack() {
+    document.body.style.backgroundColor = 'black'
+}
+function makeBlue() {
+    document.body.style.backgroundColor = 'blue'
+}
+const makeGreenButton = document.getElementById('make-green')
+makeGreenButton.onclick = makeGreen;
+
+function makeGreen() {
+    document.body.style.backgroundColor = 'green'
+}
+const makePurpleButton = document.getElementById('make-purple')
+            makePurpleButton.onclick = makePurple;
+
+            function makePurple() {
+                document.body.style.backgroundColor = 'purple'
+            }
